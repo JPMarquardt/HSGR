4c8b97f639e7a7aa3917ad7c10e5fea41104a3b8
JP Marquardt <jpaulmarquardt@icloud.com>
2024-01-19 14:48:51 -0500

    sync models

---
+++ fine_tuning_submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+#SBATCH --job-name=nfflr_train
+#SBATCH --output=logs/nfflr-%j.out
+#SBATCH --error=logs/nfflr-%j.err
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=2
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=5GB
+#SBATCH --gres=gpu
+
+module purge
+
+singularity exec --nv \
+	    --overlay /scratch/jpm9731/HSGR-singularity/my_pytorch.ext3:ro \
+	    /scratch/work/public/singularity/cuda11.6.124-cudnn8.4.0.27-devel-ubuntu20.04.4.sif\
+	    /bin/bash -c "source /ext3/env.sh; nvidia-smi; python fine_tuning.py"
