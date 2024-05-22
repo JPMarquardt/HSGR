9be7324b115ac7e53efd05af39031c9425d4adc9
John Marquardt <johnmarquardt@10-18-100-87.dynapool.wireless.nyu.edu>
2024-05-22 16:52:01 -0400

    added early return for no cutoff and colorinvemb

--- nuces/layers/utils.py
+++ nuces/layers/utils.py
@@ -19,14 +19,22 @@ class MLP(nn.Module):
         return h
     
 
-class SmoothCutoffCos(nn.Module):
+class SmoothCutoff(nn.Module):
     def __init__(self, cutoff: float = 1.0):
-        super(SmoothCutoffCos, self).__init__()
+        super(SmoothCutoff, self).__init__()
 
         self.register_buffer('pi', torch.tensor(np.pi))
         self.register_buffer('cutoff', torch.tensor(cutoff))
 
+        if not cutoff:
+            self.early_return = True
+        else:
+            self.early_return = False
+
     def forward(self, r):
+        if self.early_return:
+            return torch.ones_like(r)
+
         cutoff2 = self.cutoff ** 2
 
         v = torch.zeros_like(r)
@@ -49,10 +57,27 @@ class SmoothCutoffCos(nn.Module):
 class radial_basis_func(nn.Module):
     def __init__(self, in_feats: int = 64, in_range: tuple[float, float] = None, **kwargs):
         super(radial_basis_func, self).__init__()
-        
+
         #basis function parameters
         self.register_buffer('gamma', in_feats / (in_range[1] - in_range[0]))
         self.register_buffer('muk', torch.linspace(in_range[0], in_range[1], in_feats))
 
     def forward(self, dist):
         return torch.exp(-self.gamma * (dist - self.muk)**2)
+    
+class color_invariant_embedding(nn.Module):
+    def __init__(self, max_color: int = 10):
+        super(color_invariant_embedding, self).__init__()
+        self.register_parameter('ii', torch.tensor([1.0], requires_grad=True))
+        self.register_parameter('ij', torch.tensor([0.1], requires_grad=True))
+        
+        self.max_color = max_color
+
+    def forward(self, color, n_color):
+        ii = self.ii * torch.nn.functional.one_hot(color, self.max_color)
+        ij = self.ij * torch.ones_like(n_color)
+        
+        z = torch.zeros_like(self.max_color - n_color)
+        ij = torch.cat([ij, z], dim=-1)
+
+        return ii + ij
