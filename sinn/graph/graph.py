2ff1b738c3be19fb598f779e4e372eae97ec6bad
John Marquardt <johnmarquardt@10-18-100-87.dynapool.wireless.nyu.edu>
2024-05-28 17:00:10 -0400

    added graph functions

---
+++ sinn/graph/graph.py
@@ -0,0 +1,94 @@
+import torch
+import dgl
+import numpy as np
+
+from sinn.graph.utils import compute_dx, compute_bond_cosines, copy_d, compute_max_d, compute_nd
+
+def create_supercell(data: torch.tensor, n: int):
+    """
+    Create a supercell
+    """
+    n_atoms = data.shape[0]
+    supercell = torch.zeros((data.shape[0] * n**3, data.shape[1]))
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                ind = (i*n**2 + j*n + k) * n_atoms
+                displacement = torch.tensor([i, j, k], dtype=torch.float)
+                supercell[ind:ind + n_atoms] = data + displacement[None, :]
+
+    return supercell
+
+def lattice_plane_slicer(data: torch.tensor, miller_index: torch.tensor, n: int = 0):
+    """
+    Slice the unit cell on lattice plane
+    """
+
+    #prep miller index for normalization
+    miller_index = miller_index.float()
+    nz_miller_index = miller_index[miller_index != 0]
+
+    #normalize miller index so that the smallest component is 1
+    mi_max = torch.max(nz_miller_index)
+    miller_index = miller_index / mi_max
+
+    belowplane = torch.sum(data * miller_index, dim=1) < n
+    
+    xeq0 = miller_index[0] != 0
+    yeq0 = miller_index[1] != 0
+    zeq0 = miller_index[2] != 0
+
+    if xeq0.int() + yeq0.int() + zeq0.int() < 2:
+        return data
+    
+    #propagate in the direction of the vector with 1 because it lines up
+    max_index = torch.argmax(nz_miller_index)
+    propagation_vector = torch.zeros(3)
+    propagation_vector[max_index] = n
+    print(propagation_vector)
+
+    belowplane = belowplane.unsqueeze(1)
+    new_cell = torch.where(belowplane, data + propagation_vector, data)
+
+    return new_cell
+
+def create_knn_graph(data: torch.tensor, k: int):
+    """
+    Create a k-nearest neighbor graph with necessary edge features
+    """
+    g = dgl.knn_graph(data, k)
+    g.ndata['x'] = data
+    g.apply_edges(compute_dx)
+    g.edata['d'] = torch.norm(g.edata['dx'], dim=1)
+    g.update_all(copy_d, compute_max_d)
+    g.apply_edges(compute_nd)
+
+    tfm = dgl.LineGraph()
+    h = tfm(g)
+    h.apply_edges(compute_bond_cosines)
+
+    return (g, h)
+
+if __name__ == "__main__":
+    #verify the functions
+
+    import matplotlib.pyplot as plt
+    data = torch.rand((5, 3))
+    n = 2
+    k = 3
+    xyz = torch.tensor([1, 2, 1], dtype=torch.float)
+    xyz = xyz / torch.max(xyz)
+    supercell = create_supercell(data, n)
+    sliced_cell = lattice_plane_slicer(supercell, xyz, 2)
+    knn_graph = create_knn_graph(supercell, k)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(supercell[:, 0], supercell[:, 1], supercell[:,2], alpha=0.25)
+    ax.scatter(sliced_cell[:, 0], sliced_cell[:, 1], sliced_cell[:,2], alpha=0.25)
+    ax.plot([n/xyz[0], 0, 0, n/xyz[0]], [0, n/xyz[1], 0, 0], [0, 0, n/xyz[2], 0], color='b')
+    ax.plot([n/xyz[0], 0, 0, n/xyz[0]], [n, n+n/xyz[1], n, n], [0, 0, n/xyz[2], 0], color='r')
+
+    plt.show()
+    
+    print(knn_graph)
---
+++ sinn/graph/utils.py
@@ -0,0 +1,40 @@
+import torch
+
+def compute_dx(edges):
+    """Compute bond angle cosines from bond displacement vectors."""
+    # line graph edge: (a, b), (b, c)
+    # `a -> b -> c`
+    # 
+    r1 = -edges.src["x"]
+    r2 = edges.dst["x"]
+
+    return {"dx": r1 + r2}
+
+def compute_bond_cosines(edges):
+    """Compute bond angle cosines from bond displacement vectors."""
+    # line graph edge: (a, b), (b, c)
+    # `a -> b -> c`
+    # use law of cosines to compute angles cosines
+    # negate src bond so displacements are like `a <- b -> c`
+    # cos(theta) = ba \dot bc / (||ba|| ||bc||)
+    r1 = -edges.src["dx"]
+    r2 = edges.dst["dx"]
+    bond_cosine = torch.sum(r1 * r2, dim=1) / (
+        torch.norm(r1, dim=1) * torch.norm(r2, dim=1)
+    )
+    bond_cosine = torch.clamp(bond_cosine, -1, 1)
+    # bond_cosine = torch.arccos((torch.clamp(bond_cosine, -1, 1)))
+
+    return {"cos": bond_cosine}
+
+def copy_d(edges):
+    """Copy the bond distance from the original graph to the line graph."""
+    return {"d": edges.data["d"]}
+
+def compute_max_d(nodes):
+    """Compute the maximum distance between a node and its neighbors."""
+    return {"max_d": torch.max(nodes.mailbox["d"], dim=1)[0]}
+
+def compute_nd(edges):
+    """Compute the normalized distance between two atoms."""
+    return {"nd": edges.data["d"] / edges.dst["max_d"]}
