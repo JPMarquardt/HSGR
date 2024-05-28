cfc15a9d6a2c8e5f4d5fde971be809dcf0f29202
John Marquardt <johnmarquardt@10-18-100-87.dynapool.wireless.nyu.edu>
2024-05-28 17:15:56 -0400

    added schnet embedding

--- sinn/layers/embeddings.py
+++ sinn/layers/embeddings.py
@@ -6,38 +6,6 @@ import torch.nn as nn
 from sinn.layers.utils import SmoothCutoff, MLP, radial_basis_func
 from typing import (Any, Dict, List, Literal, Tuple, Union, Optional, Callable)
 
-def compute_delta_radius(edges):
-    """Compute bond angle cosines from bond displacement vectors."""
-    # line graph edge: (a, b), (b, c)
-    # `a -> b -> c`
-    # 
-    r1 = -edges.src["nr"]
-    r2 = edges.dst["nr"]
-    r1 = torch.norm(r1, dim=1)
-    r2 = torch.norm(r2, dim=1)
-    # bond_cosine = torch.arccos((torch.clamp(bond_cosine, -1, 1)))
-
-    delta_radius = r2-r1
-
-    return {"dnr": delta_radius}
-
-def compute_bond_cosines(edges):
-    """Compute bond angle cosines from bond displacement vectors."""
-    # line graph edge: (a, b), (b, c)
-    # `a -> b -> c`
-    # use law of cosines to compute angles cosines
-    # negate src bond so displacements are like `a <- b -> c`
-    # cos(theta) = ba \dot bc / (||ba|| ||bc||)
-    r1 = -edges.src["r"]
-    r2 = edges.dst["r"]
-    bond_cosine = torch.sum(r1 * r2, dim=1) / (
-        torch.norm(r1, dim=1) * torch.norm(r2, dim=1)
-    )
-    bond_cosine = torch.clamp(bond_cosine, -1, 1)
-    # bond_cosine = torch.arccos((torch.clamp(bond_cosine, -1, 1)))
-
-    return {"h": bond_cosine}
-
 """
 class nplet_embedding(nn.Module):
 
@@ -120,3 +88,73 @@ class color_invariant_triplet(nn.Module):
         hbc = h.edata['hbc']
 
         return self.e1(hac) + self.e2(hab) + self.e3(hbc)
+
+class SchnetEmbedding(nn.Module):
+    """
+    Graph convolution layer from SchNet model (adds edge features too)
+    Inputs:
+    - in_feats: int, input features
+    - out_feats: int, output features
+    - var: str, variable to use for convolution
+    Forward:
+    - g: dgl.DGLGraph, input graph
+    Outputs:
+    - out: torch.tensor, output features
+    """
+    def __init__(self, 
+                 in_feats: int = 64,
+                 radial_feats: int = 128,
+                 out_feats: int = 64, 
+                 var: str = 'd', 
+                 cutoff: bool = True, 
+                 in_range: tuple[float, float] = None, 
+                 **kwargs):
+        super(SchnetEmbedding, self).__init__()
+        self.var = var
+
+        #cutoff function
+        if cutoff:
+            max = in_range[1]
+            self.cutoff = SmoothCutoff(cutoff=max)
+        else:
+            self.cutoff = SmoothCutoff(cutoff=False)
+
+        #initialize radial basis function
+        self.basis_func = radial_basis_func(radial_feats, in_range)
+        
+        #filter generation network
+        self.FGN_MLP1 = MLP(radial_feats, in_feats)
+        self.FGN_MLP2 = MLP(in_feats, in_feats)
+
+        #interaction block
+        self.IB_MLP = MLP(in_feats, out_feats)
+
+    def cfconv(self, edges):
+        edge_feat = edges.data['h']
+
+        cutoff = edges.data['cutoff']
+        bf = edges.data['bf']
+
+        return {'h': edge_feat * bf * cutoff.unsqueeze(-1)}
+
+    def reduce_func(self, nodes):
+        return {'h': torch.prod(nodes.mailbox['h'], dim=1)}
+
+    def forward(self, g):
+        g = g.local_var()
+
+        e_var = g.edata[self.var]
+
+        if g.edata.get('cutoff') is None:
+            bf = self.basis_func(e_var)
+            cutoff = self.cutoff(e_var)
+
+            g.edata['bf'] = bf * cutoff
+            g.edata['cutoff'] = cutoff
+
+        bf = self.FGN_MLP1(bf)
+        bf = self.FGN_MLP2(bf)
+
+        g.update_all(self.cfconv, self.reduce_func)
+        out = self.IB_MLP(g.ndata['h'])
+        return out
