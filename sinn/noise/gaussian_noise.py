98d7efa287c959d752cd63db6664ecac9e04143e
John Marquardt <johnmarquardt@10-18-100-87.dynapool.wireless.nyu.edu>
2024-05-28 17:11:25 -0400

    finished noise_regression

--- sinn/noise/gaussian_noise.py
+++ sinn/noise/gaussian_noise.py
@@ -1,23 +1,23 @@
 import torch
 from sinn.noise.utils import dist_matrix, find_min_dist
 
-def gaussian_noise(tensor, noise):
+def gaussian_noise(tensor, noise_size):
     """Add Gaussian noise to a tensor.
 
     Args:
         tensor (torch.Tensor): Input tensor.
-        ratio (float, optional): Ratio of the standard deviation of the Gaussian noise to the mean distance between atoms
+        noise (torch): The magnitude of the noise
 
     Returns:
         torch.Tensor: Noisy tensor.
     """
     min_dist = find_min_dist(tensor)
 
-    return tensor + noise * min_dist
+    return tensor + torch.randn(tensor.shape) * min_dist * noise_size
     
 def noise_regression(tensor):
     noise_stdev = torch.rand()
-    noise = torch.randn(tensor.shape) * noise_stdev
+    noisy_tensor = gaussian_noise(tensor, noise_stdev)
 
     return (noise_stdev, noisy_tensor)
     
