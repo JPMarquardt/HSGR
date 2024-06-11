89d4982c70ac879fff711ac91a6a231127069986
JPMarquardt <jpaulmarquardt@icloud.com>
2024-06-10 21:23:22 -0400

    preped for cscl

--- sinn/examples/SINN_SIM.py
+++ sinn/examples/SINN_SIM.py
@@ -1,41 +1,37 @@
 import torch.nn as nn
 import torch
+import matplotlib.pyplot as plt
 
-from MDAnalysis.coordinates.GSD import GSDReader
 from MDAnalysis import Universe
 
 from sinn.dataset.dataset import FilteredAtomsDataset
 from sinn.model.model import SchNet, Alignn
-from sinn.train.train import run_epoch
+from sinn.train.train import test_model
 from sinn.train.transforms import SimulatedNoiseRegressionEval
 
-n_atoms = 2
-spg = ('225',)
-categorical_filter = ([True],['spg_number'],[spg])
-
-batch_size = 8
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 k = 50
 pre_eval_func = SimulatedNoiseRegressionEval(k = k)
 
-dataset = Universe('./test_traj/trajectory_LS4_FP0.5_RN105_BL10_DL5.3_Th3P4.gsd')
+dataset_CsCl = Universe('./test_traj/trajectory_LS5.5_FP0.5_RN105_BL10_DL5.4_CsCl.gsd')
+dataset_agg = Universe('./trajectory_LS4.5_FP0.5_RN105_BL10_DL5.35_aggregate.gsd')
 
-dataset = FilteredAtomsDataset(source = dataset,
+dataset = FilteredAtomsDataset(source = dataset_CsCl,
                                transform=pre_eval_func,
                                target = 'target').dataset
 
 model_name = 'SchNet-AtomNoise-Spg225'
-model_path = 'models/24-05-29/'
+model_path = 'models/24-06-10/'
 output_dir = f'{model_path}{model_name}.pkl'
 model = torch.load(output_dir)
+
 loss_func = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-run_epoch(model = model, 
-          loader = dataset, 
-          loss_func=loss_func, 
-          optimizer = optimizer, 
-          device=device, 
-          epoch = 0, 
-          train=False,)
+pred_list = test_model(model = model, 
+                       dataset=dataset,
+                       device=device,)
+
+plt.plot(pred_list)
+plt.savefig(f'{model_path}{model_name}_CsCl.png')
