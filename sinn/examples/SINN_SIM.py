b378bf14035d81338121b6cf3f3d62239b74bedf
JPMarquardt <jpaulmarquardt@icloud.com>
2024-06-11 13:53:59 -0400

    added format

--- sinn/examples/SINN_SIM.py
+++ sinn/examples/SINN_SIM.py
@@ -14,7 +14,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 k = 17
 pre_eval_func = SimulatedNoiseRegressionEval(k = k)
 
-dataset_CsCl = Universe('./test_traj/trajectory_LS5.5_FP0.5_RN105_BL10_DL5.4_CsCl.gsd')
+dataset_CsCl = Universe('./test_traj/trajectory_LS5.5_FP0.5_RN105_BL10_DL5.4_CsCl.gsd', topology_format='GSD')
 
 dataset = FilteredAtomsDataset(source = dataset_CsCl,
                                transform=pre_eval_func,
