193e1faceee8c1fed7b49ffb5a65b5238e642e63
JPMarquardt <jpaulmarquardt@icloud.com>
2024-06-11 13:46:46 -0400

    CsCl

--- sinn/examples/SINN_SIM.py
+++ sinn/examples/SINN_SIM.py
@@ -14,9 +14,9 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 k = 17
 pre_eval_func = SimulatedNoiseRegressionEval(k = k)
 
-dataset_aggr = Universe('./test_traj/trajectory_LS4.5_FP0.5_RN105_BL10_DL5.35_aggr')
+dataset_CsCl = Universe('./test_traj/trajectory_LS5.5_FP0.5_RN105_BL10_DL5.4_CsCl.gsd')
 
-dataset = FilteredAtomsDataset(source = dataset_aggr,
+dataset = FilteredAtomsDataset(source = dataset_CsCl,
                                transform=pre_eval_func,
                                target = 'target').dataset
 
@@ -33,4 +33,4 @@ pred_list = test_model(model = model,
                        device=device,)
 
 plt.plot(pred_list)
-plt.savefig(f'{model_path}{model_name}_aggr.png')
+plt.savefig(f'{model_path}{model_name}_CsCl.png')
