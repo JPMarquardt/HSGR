3c77bef84c919b744ca673806869c9be21a66409
JPMarquardt <jpaulmarquardt@icloud.com>
2024-06-25 15:36:29 -0400

    n milestones

--- sinn/examples/SCHNET.py
+++ sinn/examples/SCHNET.py
@@ -44,8 +44,8 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 scheduler1 = torch.optim.lr_scheduler.ConstantLR(optimizer, factor=0.1, total_iters=20)
 scheduler2 = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9, last_epoch=-1)
-scheduler3 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10, eta_min=0.0001)
-scheduler = torch.optim.lr_scheduler.SequentialLR(optimizer, [scheduler1, scheduler2,scheduler3], milestones=[20, 40, 60])
+scheduler3 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=15, eta_min=0.0001)
+scheduler = torch.optim.lr_scheduler.SequentialLR(optimizer, [scheduler1, scheduler2,scheduler3], milestones=[20, 40])
 
 train_model(model = model,
             dataset = dataset,
