bd6f49d0b599a0f8c8cffb43ece78a7d4549d06e
JPMarquardt <jpaulmarquardt@icloud.com>
2024-06-27 21:00:23 -0400

    moved box filters

--- sinn/graph/graph.py
+++ sinn/graph/graph.py
@@ -154,6 +154,36 @@ def create_periodic_graph(g):
 
     return g
 
+def big_box_filter(data, lattice, dx):
+    d = data.size()[1]
+    full_filter = torch.ones(data.size()[0], dtype=torch.bool)
+
+    for i in range(d):
+        lattice_vector = lattice[i]
+        lattice_vector_norm = torch.norm(lattice_vector)
+        dp = torch.sum(data * lattice_vector/lattice_vector_norm, dim=1)
+        high_filt = dp < 2 * lattice_vector_norm + dx
+        low_filt = dp > lattice_vector_norm - dx
+        filt = torch.logical_and(high_filt, low_filt)
+        full_filter = torch.logical_and(full_filter, filt)
+
+    return full_filter
+
+def small_box_filter(data, lattice, center):
+    d = data.size()[1]
+    full_filter = torch.ones(data.size()[0], dtype=torch.bool)
+
+    for i in range(d):
+        lattice_vector = lattice[i]
+        lattice_vector_norm = torch.norm(lattice_vector)
+        dp = torch.sum(data * lattice_vector/lattice_vector_norm, dim=1)
+        high_filt = dp < center * lattice_vector_norm + 1
+        low_filt = dp > center * lattice_vector_norm
+        filt = torch.logical_and(high_filt, low_filt)
+        full_filter = torch.logical_and(full_filter, filt)
+
+    return full_filter
+
 if __name__ == "__main__":
     #verify the functions
 
