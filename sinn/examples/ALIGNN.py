917aec4fb1f53b8f77fb7cec1e78ce798b4263cb
JPMarquardt <jpaulmarquardt@icloud.com>
2024-07-25 15:05:29 -0400

    update

--- sinn/examples/ALIGNN.py
+++ sinn/examples/ALIGNN.py
@@ -12,7 +12,7 @@ from sinn.train.loss import RegressionClassificationLoss, find_class_weights
 date = datetime.now().strftime("%y-%m-%d")
 
 n_atoms = 2
-spg = list(range(1,231))
+spg = list(range(195,231))
 
 categorical_filter = ([True],['spg_number'],[spg])
 
