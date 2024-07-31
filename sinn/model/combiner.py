3e648f1503360f0d6f63b488e619cf8d7346115d
JPMarquardt <jpaulmarquardt@icloud.com>
2024-07-31 16:42:13 -0400

    fixed dims

--- sinn/model/combiner.py
+++ sinn/model/combiner.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import torch
 from typing import List, Tuple, Callable
 from sklearn.decomposition import IncrementalPCA
+#from torchmdnet.models.model import TorchMD_Net
 
 
 
@@ -12,13 +13,13 @@ class Model_Combiner(nn.Module):
         self.model = model
         model.model.fc.register_forward_hook(self.hook_fn)
 
-        self.register_buffer('pca_weights', pca)
+        self.register_buffer('pca_weights', torch.transpose(pca, 0, 1))
 
     def hook_fn(self, module, input, output):
-        self.output = output
+        self.output: torch.Tensor = output
 
-    def forward(self, x):
+    def forward(self, x):       
         x = self.pre_eval_func(x)
-        x = self.model(x)
-        
-        return torch.sum(self.pca_weights * self.output)
+        self.model(x)
+
+        return torch.sum(self.pca_weights[None, :, :] * self.output[:, :, None], dim=1)
