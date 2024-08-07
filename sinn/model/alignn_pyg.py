929b056db49ad2413450700fd70f862d68422975
JPMarquardt <jpaulmarquardt@icloud.com>
2024-08-07 05:20:55 -0400

    combind

--- sinn/model/alignn_pyg.py
+++ sinn/model/alignn_pyg.py
@@ -78,7 +78,6 @@ class Alignn(nn.Module):
         x = torch.sum(x, dim=0)
         x = self.fc2(x)
 
-        if self.kwargs.get('classification'):
-            return F.softmax(x, dim=-1)
+        return F.softmax(x, dim=-1)
         
         return x.squeeze()
