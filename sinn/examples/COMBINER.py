dc7159e67ae4ef0b47abf568b7f11aa480d38459
JPMarquardt <jpaulmarquardt@icloud.com>
2024-08-16 23:22:00 -0400

    update

--- sinn/examples/COMBINER.py
+++ sinn/examples/COMBINER.py
@@ -8,7 +8,7 @@ from sinn.train.transforms_pyg import AperiodicKNN_PyG
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model_path = f'models/24-08-15/'
+model_path = f'models/24-08-16/'
 date = model_path.split('/')[-2]
 model_name = f'Alignn-k17-L4-spg22-n7'
 
