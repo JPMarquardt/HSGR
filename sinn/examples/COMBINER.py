ccc617da52420a6ed8da485e1146c3c1a82aeac2
JPMarquardt <jpaulmarquardt@icloud.com>
2024-08-21 00:07:38 -0400

    started adding argparser

--- sinn/examples/COMBINER.py
+++ sinn/examples/COMBINER.py
@@ -1,4 +1,5 @@
 import pickle
+import argparse
 import torch
 from sklearn.decomposition import IncrementalPCA
 
@@ -6,21 +7,29 @@ from sinn.model.combiner import ModelCombiner
 from sinn.model.alignn_pyg import Alignn
 from sinn.train.transforms_pyg import AperiodicKNN_PyG
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model_path = f'models/24-08-19/'
-date = model_path.split('/')[-2]
-model_name = f'Alignn-k17-L4-spg22-n8'
+def main(model_path):
+    model_name = model_path.split('/')[-1].split('.')[0]
+    model_path = model_path.split(model_name)[0]
 
-base_model = torch.load(f'{model_path}{model_name}.pkl', map_location=device)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-k = int(model_name.split('-')[1].split('k')[1])
-pre_eval_func = AperiodicKNN_PyG(k = k)
+    base_model = torch.load(f'{model_path}{model_name}.pkl', map_location=device)
 
-indices = [-7, -8]
+    k = int(model_name.split('-')[1].split('k')[1])
+    pre_eval_func = AperiodicKNN_PyG(k = k)
 
-for i in range(2):
-    #model = Model_Combiner(pre_eval_func=pre_eval_func, model=base_model, pca=pca_model[i].unsqueeze(0))
-    model = ModelCombiner(pre_eval_func=pre_eval_func, model=base_model, index=indices[i])
-    model.eval()
-    torch.save(model, f'simulations/{date}/{model_name}-combiner{i}.pkl')
+    indices = [-7, -8]
+
+    for i in range(2):
+        #model = Model_Combiner(pre_eval_func=pre_eval_func, model=base_model, pca=pca_model[i].unsqueeze(0))
+        model = ModelCombiner(pre_eval_func=pre_eval_func, model=base_model, index=indices[i])
+        model.eval()
+        torch.save(model, f'simulations/{date}/{model_name}-combiner{i}.pkl')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Create a directory for umbrella sampling of a model')
+    parser.add_argument('model_path', type=str, help='Path to the model file')
+    args = parser.parse_args()
+
+    main(args.model_path)
