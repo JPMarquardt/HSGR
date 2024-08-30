f8cfdf2e744e09294f4b99e750518747f38348b4
JPMarquardt <jpaulmarquardt@icloud.com>
2024-08-30 15:49:57 -0400

    update

--- sinn/examples/GET-CV-LOC.py
+++ sinn/examples/GET-CV-LOC.py
@@ -30,7 +30,7 @@ def main(model_path):
     dataset_names = ['CsCl.gsd', 'aggr.gsd', 'Th3P4.gsd']
     sparsity = [1000, 1000, 1000]
 
-    pos_dims = [16, [15, 16, 15]]
+    pos_dims = [15, [14, 15, 14]]
     neg_dims = [None, 'all']
     n_interpolation_points = 5
     mean_tensor = torch.zeros((len(dataset_names), len(pos_dims)))
