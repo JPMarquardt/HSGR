2ec4d39233702893e30c582bb6691947c63e2d43
JPMarquardt <jpaulmarquardt@icloud.com>
2024-10-08 03:27:23 -0400

    updated fig to make easier to spam

---
+++ sinn/examples/EVALAUTE.py
@@ -0,0 +1,85 @@
+import torch.nn as nn
+import torch
+import importlib
+import argparse
+
+from MDAnalysis.coordinates.GSD import GSDReader
+from MDAnalysis import Universe
+
+from sinn.dataset.dataset import FilteredAtomsDataset
+from sinn.model.alignn_pyg import Alignn
+from sinn.train.train import test_model
+from sinn.train.transforms_pyg import AperiodicKNN_PyG
+import os
+
+def main(paths):
+    new_paths = []
+    for path in paths:
+        if path.endswith('/'):
+            folder_path = os.path.dirname(path)
+            for file in os.listdir(folder_path):
+                if file.endswith('.pkl'):
+                    new_paths.append(os.path.join(folder_path, file))
+        elif path.endswith('.pkl'):
+            new_paths.append(path)
+        else:
+            print('Invalid path')
+
+    paths = new_paths
+    print(paths)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    model_names = [path.split('/')[-1].split('.')[0] for path in paths]
+    dates = [path.split('/')[-2] for path in paths]
+
+    for i, model_name in enumerate(model_names):
+        model_path = f'models/{dates[i]}/'
+        k = int(model_name.split('-')[1].split('k')[1])
+        print(k)
+        pre_eval_func = AperiodicKNN_PyG(k = k)
+
+        model = torch.load(model_path + model_name + '.pkl', map_location=device)
+
+        dataset_names = ['CsCl.gsd', 'Th3P4.gsd', 'aggr.gsd']
+        sparsity = [100, 100, 100]
+
+        for n, name in enumerate(dataset_names):
+            dataset = Universe(f'./test_traj/{name}')
+
+            dataset = FilteredAtomsDataset(source = dataset,
+                                        transform=pre_eval_func,
+                                        target = 'target',
+                                        sparsity=sparsity[n])
+            
+
+            def hook_fn(module, input, output):
+                fc2.append(output)
+            model.fc.register_forward_hook(hook_fn)
+
+            fc2 = []
+            
+            preds = test_model(model = model, 
+                            dataset=dataset,
+                            device=device,)
+
+            fc_save = torch.stack(fc2, dim=0)
+            preds_save = torch.stack(preds)
+
+            print(preds_save.shape)
+            print(fc_save.shape)
+
+            name = name.split('.')[0]
+            torch.save(fc_save, model_path + model_name + f'-{name}_fc2.pkl')
+            torch.save(preds_save, model_path + model_name + f'-{name}_preds.pkl')
+
+            fc_save = None
+            preds_save = None
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Evaluate a model on a dataset')
+    parser.add_argument('paths', type=str, nargs='+', help='Paths to the model files')
+
+    args = parser.parse_args()
+    main(args.paths)
--- sinn/examples/FIG.py
+++ sinn/examples/FIG.py
@@ -1,47 +1,99 @@
+import os
+import argparse
+
 import matplotlib.pyplot as plt
 import torch
 from sklearn.decomposition import PCA
 from sinn.train.transforms_pyg import PeriodicKNN_PyG
 from sinn.dataset.dataset import FilteredAtomsDataset
 
+class DataPlotter():
+    def __init__(self, path):
+        if not path.endswith('.pkl'):
+            raise ValueError('Invalid path')
+        
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-names = ['CsCl.gsd', 'Th3P4.gsd', 'aggr.gsd']
-shapes = ['o', 's', 'x']
-dates = [f'2024-09-16', f'2024-09-16', f'2024-09-16', f'2024-09-18']
-model_names = [f'Alignn-k19-L3-spg22-n8', f'Alignn-k19-L4-spg22-n8', f'Alignn-k19-L4-int5-n8', f'Alignn-k19-L3-int5-n8']
+        self.paths = path
+        self.model = '-'.join(path.split('/')[-1].split('_')[0].split('-')[:-2])
+        self.date = path.split('/')[-2]
+        self.target = path.split('/')[-1].split('_')[0].split('-')[-1]
+        self.data_type = path.split('/')[-1].split('_')[1].split('.')[0]
 
-for i, model_name in enumerate(model_names):
-    pca = PCA(n_components=2)
-    plt.figure()
-    for n, crystal_name in enumerate(names):
-        data = torch.load(f'models/{dates[i]}/{model_name}-{crystal_name}_fc2.pkl', map_location=device).squeeze()
-        print(data.shape)
-        data = data.reshape(-1, data.shape[-1] * data.shape[-2])
-        print(data[0])
-        print(data[-1])
-        print(data.shape)
-        data = data.cpu().detach().numpy()
-        if n == 0:
-            for_plotting = pca.fit_transform(data)
+        self.data: torch.Tensor = torch.load(path, map_location=device)
+
+        if self.data_type == 'fc2':
+            self.data = self.data.squeeze()
+            self.data = self.data.reshape(-1, self.data.shape[-1] * self.data.shape[-2])
+            self.data = self.data.cpu().detach().numpy()
+
+        self.idenfier = ( self.date, self.model, self.data_type)
+
+
+    def set_figure(self, figure: plt.Figure):
+        self.figure = figure
+
+    def set_pca(self, pca: PCA):
+        self.pca = pca
+
+
+
+def main(paths):
+    target_shape_map = {
+        'CsCl.gsd': 'o',
+        'Th3P4.gsd': 's',
+        'aggr.gsd': 'x'
+    }
+
+    new_paths = []
+    for path in paths:
+        if path.endswith('/'):
+            folder_path = os.path.dirname(path)
+            for file in os.listdir(folder_path):
+                if file.endswith('fc2.pkl') or file.endswith('preds.pkl'):
+                    paths.append(os.path.join(folder_path, file))
+        elif path.endswith('.pkl'):
+            new_paths.append(path)
         else:
-            for_plotting = pca.transform(data)
-        print(for_plotting.shape)
-        colors = torch.linspace(0,1,for_plotting.shape[0])
-        plt.scatter(for_plotting[:,0], for_plotting[:,1], label=crystal_name, marker=shapes[n], c=colors, alpha=0.5, cmap='viridis')
-        plt.legend()
-    plt.savefig(f'models/{dates[i]}/{model_name}-scatter.png')
-    print(f'models/{dates[i]}/{model_name}-scatter.png')
+            print('Invalid path')
 
-for i, model_name in enumerate(model_names):
-    plt.figure()
-    for n, crystal_name in enumerate(names):
-        data = torch.load(f'models/{dates[i]}/{model_name}-{crystal_name}_preds.pkl', map_location=device).squeeze()
-        for_plotting = data.cpu().detach().numpy()
-        print(for_plotting.shape)
-        print(for_plotting[0])
-        colors = torch.linspace(0,1,for_plotting.shape[0])
-        plt.scatter(for_plotting[:,0], for_plotting[:,-1], label=crystal_name, marker=shapes[n], c=colors, alpha=0.5, cmap='viridis')
-        plt.legend()
-    plt.savefig(f'models/{dates[i]}/{model_name}-scatter-preds.png')
-    print(f'models/{dates[i]}/{model_name}-scatter-preds.png')
+
+    data = [DataPlotter(path) for path in new_paths]
+
+    plotted: dict[tuple[str], DataPlotter] = {}
+
+    for i, data in enumerate(data):
+        if data.idenfier not in plotted:
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            data.set_figure(fig)
+
+            plotted[data.idenfier] = data
+
+            if data.data_type == 'fc2':
+                pca = PCA(n_components=2)
+                data.data = pca.fit_transform(data.data)
+
+                data.set_pca(pca)
+        else:
+            fig = plotted[data.idenfier].figure
+            ax = fig.get_axes()[0]
+
+            if data.data_type == 'fc2':
+                data.data = plotted[data.idenfier].pca.transform(data.data)
+
+        colors = torch.linspace(0,1,data.data.shape[0])
+        ax.scatter(data.data[:,0], data.data[:,-1], label=data.target, marker=target_shape_map[data.target], c=colors, alpha=0.5, cmap='viridis')
+        fig.legend()
+
+    for data in plotted.values():
+        data.figure.savefig(f'models/{data.date}/{data.model}-{data.data_type}-scatter.png')
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Evaluate a model on a dataset')
+    parser.add_argument('paths', type=str, nargs='+', help='Paths to the model files')
+
+    args = parser.parse_args()
+    main(args.paths)
