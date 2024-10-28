5b47e3f99152b1d3fce4be7acf5d27266998b30e
JPMarquardt <jpaulmarquardt@icloud.com>
2024-10-28 02:49:31 -0400

    full folder model combining

--- sinn/examples/COMBINER.py
+++ sinn/examples/COMBINER.py
@@ -2,6 +2,8 @@ import pickle
 import argparse
 import torch
 from sklearn.decomposition import IncrementalPCA
+import os
+import pathlib
 
 from sinn.model.combiner import ModelCombiner
 from sinn.model.alignn_pyg import Alignn
@@ -13,11 +15,16 @@ def main(model_path):
         date = model_path.split('/')[-2]
         model_name = model_path.split('/')[-1].split('.')[0]
         model_path = model_path.split(model_name)[0]
+        print(date, model_name, model_path)
 
     if model_path.endswith('/'):
-        date = model_path.split('/')[-3]
-        model_name = model_path.split('/')[-1]
-        model_path = model_path.split('/')[-2]
+        files = os.listdir(model_path)
+        files = [f for f in files if f.endswith('.pkl')]
+
+        for f in files:
+            full_path = f'{model_path}{f}'
+            main(full_path)
+
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -26,13 +33,16 @@ def main(model_path):
     k = int(model_name.split('-')[1].split('k')[1])
     pre_eval_func = AperiodicKNN_PyG(k = k)
 
-    indices = [-7, -8]
+    if model_name.split('-')[-2][:3] == 'int':
+        indices = [-1, -5]
+    elif model_name.split('-')[-2][:3] == 'spg':
+        indices = [-7, -8]
 
     for i in range(2):
         #model = Model_Combiner(pre_eval_func=pre_eval_func, model=base_model, pca=pca_model[i].unsqueeze(0))
         model = ModelCombiner(pre_eval_func=pre_eval_func, model=base_model, index=indices[i])
         model.eval()
-        torch.save(model, f'models/{date}/{model_name}-combiner{i}.pkl')
+        torch.save(model, f'simulations/{date}/{model_name}-combiner{i}.pkl')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create a directory for umbrella sampling of a model')
