6db09a8dea05a85366a22841fb35527657365588
JPMarquardt <jpaulmarquardt@icloud.com>
2024-10-30 00:02:01 -0400

    typing fix

--- sinn/graph/graph_pyg.py
+++ sinn/graph/graph_pyg.py
@@ -78,8 +78,8 @@ def create_labeled_supercell(data: torch.Tensor, n: int, lattice: torch.Tensor =
     norm_props = torch.ceil(lattice_vector_norm_prop * n).int()
 
     # create the atom ids
-    atom_id = torch.arange(n_atoms, dtype=torch.int, device=device)
-    atom_id = atom_id.repeat(n**3)
+    atom_id = torch.arange(n_atoms, dtype=torch.float(), device=device)
+    atom_id = atom_id.repeat(n**3).int()
 
     # create the cell ids
     cell_id_1 = torch.arange(n, dtype=torch.int, device=device).repeat_interleave(n_atoms * n**2)
